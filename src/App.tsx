dce99a006228c92cab88cde681ae591881578448
nobiart <dmitri.besleaga@gmail.com>
2026-04-21 23:58:03 +0300

    chore: add classNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { NavLink, Outlet } from "react-router";
 import { Counter } from "./components/Counter";
 import "./styles/index.scss";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export default function App() {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames("app", {}, [theme])}>
             Hello, World!
             <Counter />
             <button onClick={toggleTheme}>Change theme</button>
