29cd79d11d675d57faaf9d6eea42247838025805
nobiart <dmitri.besleaga@gmail.com>
2026-04-23 19:35:50 +0300

    chore: add router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { NavLink, Outlet } from "react-router";
 import "./styles/index.scss";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "./providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
 
 export default function App() {
     const {theme, toggleTheme} = useTheme();
@@ -12,6 +13,7 @@ export default function App() {
             <button onClick={toggleTheme}>Change theme</button>
             <NavLink to="/">Main</NavLink>
             <NavLink to="/about">About</NavLink>
+            <AppRouter />
             <Outlet />
         </div>
     )
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,30 +1,14 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { Suspense } from "react";
+import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "app/providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import App from "./App";
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root!).render(
  <BrowserRouter>
     <ThemeProvider>
-        <Routes>
-            <Route path="/" element={<App />}>
-                <Route index element={
-                    <Suspense fallback={<div>loading...</div>}>
-                        <MainPage />
-                    </Suspense>
-                } />
-                <Route path="/about" element={
-                    <Suspense fallback={<div>loading...</div>}>
-                        <AboutPage />
-                    </Suspense>
-                } />
-            </Route>
-        </Routes>
+        <App />
     </ThemeProvider>
  </BrowserRouter>
 )
