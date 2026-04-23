d5190e43d6f80db9eb2a2e00ab1db9778864e4fb
nobiart <dmitri.besleaga@gmail.com>
2026-04-23 23:45:48 +0300

    feat: add Button UI component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,21 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import cls from "./Button.module.scss";
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+export enum ThemeButton {
+    CLEAR = "clear",
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: ReactNode;
+    className?: string;
+    theme?: ThemeButton;
+}
+
+export const Button = ({ children, className = "", theme = ThemeButton.CLEAR, ...rest }: ButtonProps) => {
+  return (
+    <button className={classNames(cls.button, {}, [className, cls[theme]])} {...rest}>
+        {children}
+    </button>
+  );
+};
