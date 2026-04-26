16cc999bba38fdb101f5600cb7a463421f54f300
nobiart <dmitri.besleaga@gmail.com>
2026-04-26 22:28:20 +0300

    feat: add Not found page

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+import { useTranslation } from "react-i18next";
+import cls from "./NotFoundPage.module.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+
+interface NotFoundPageProps {
+  className?: string;
+}
+
+export const NotFoundPage = ({ className = "" }: NotFoundPageProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(cls.notFound, {}, [className])}>
+      {t("notFoundPage")}
+    </div>
+  );
+};
